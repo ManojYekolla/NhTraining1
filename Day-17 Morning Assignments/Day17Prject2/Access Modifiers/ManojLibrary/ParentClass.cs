b7f485d6b602209dab9c1966364ad263ba0a4be9
ManojYekolla <manojyekolla93@gmail.com>
2022-02-15 17:41:49 +0530

    Day17 Assignment

---
+++ Day-17 Morning Assignments/Day17Prject2/Access Modifiers/ManojLibrary/ParentClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers
+{
+    public class ParentClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+
+        public void MyParentClass()
+        {
+            a = 3;
+            b = 4;
+            c = 5;
+            d = 6;
+            e = 7;
+        }
+    }
+    public class ChildClass : ParentClass
+    {
+        public void MyChildClass()
+        {
+           /* a = 3;
+            b = 4;
+            c = 5;
+            d = 6;
+            e = 7;*/
+        }
+    }
+    public class OtherClass
+    {
+        public void MyOtherClass()
+        {
+            ParentClass obj = new ParentClass();
+
+           /* obj.a = 3;
+            obj.b = 4;
+            obj.c = 5;
+            obj.d = 6;
+            obj.e = 7;*/
+        }
+    }
+}
