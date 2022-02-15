f826f174160469568368065de1f9005b86e2487f
ManojYekolla <manojyekolla93@gmail.com>
2022-02-15 10:40:04 +0530

    Day16 Morning Assignments Updated

---
+++ Day-16 Morning Assignments/Partial Class-example/ManojLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManojLibrary1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Math1.Add(1, 2)); 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day-16 Morning Assignments/Partial Class-example/ManojLibrary1/ManojLibrary1/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManojLibrary1
+{
+    public  static partial class Math1
+    {
+        public static int Divide(int a,int b)
+        {
+            return a / b;
+        }
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
