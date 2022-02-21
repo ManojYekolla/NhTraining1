b5dfca61b46ecc7fec087d67678a9c4b09a68aa4
ManojYekolla <manojyekolla93@gmail.com>
2022-02-21 17:32:52 +0530

    Day21 Update Submisson

---
+++ Day-21 Morning Assignments/Day21Project1/MyClientApp/MyClientApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClientApp.ServiceReference1;
+
+namespace MyClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlgebraSoapClient obj = new AlgebraSoapClient();
+
+            Console.WriteLine("Factorial Number 5 is {0}:",obj.Factorial(5));
+            Console.WriteLine("Add two Numbers 5 and 4 is {0}:", obj.Add(5,4));
+            Console.WriteLine("Mul two Numbers 8 and 3is {0}:", obj.Mul(8,3));
+            Console.WriteLine("Div two Numbers 6 and 4 is {0}:", obj.Div(6,4));
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
---
+++ Day-21 Morning Assignments/Day21Project2/WindowsFormsApp1(Factorial)/WindowsFormsApp1(Factorial)/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1_Factorial_.ServiceReference1;
+
+namespace WindowsFormsApp1_Factorial_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n=Convert.ToInt32(textBox1.Text);
+            WebService1SoapClient obj = new WebService1SoapClient();
+            textBox2.Text=obj.Factorial(n).ToString();
+
+
+        }
+    }
+}
